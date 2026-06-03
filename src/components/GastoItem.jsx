78c9e1d96bdd2a83baac5503ff7fd13f2edd4266
geronimo-ascani <geronimoascani@gmail.com>
2026-06-02 21:19:34 -0300

    GastoItem y GastoList

---
+++ src/components/GastoItem.jsx
@@ -0,0 +1,33 @@
+import { eliminarGasto } from '../services/gastosService';
+
+function GastoItem({gasto, onGastoEliminado}){
+    const handleEliminar = async () => {
+        const confirmar = window.confirm('Eliminar "${gasto.descripcion}"?');
+
+        if (!confirmar) return;
+
+        try{
+            await eliminarGasto(gasto.id);
+            onGastoEliminado(gasto.id);
+        } catch (error){
+            alert('Error al eliminar el gasto');
+        }
+    };
+
+
+    return(
+         <div className="gasto-item">
+      <div className="gasto-info">
+        <span className="gasto-descripcion">{gasto.descripcion}</span>
+        <span className="gasto-categoria">{gasto.categoria}</span>
+        <span className="gasto-fecha">{gasto.fecha}</span>
+      </div>
+      <div className="gasto-acciones">
+        <span className="gasto-monto">${gasto.monto}</span>
+        <button onClick={handleEliminar}>Eliminar</button>
+      </div>
+    </div>
+    );
+}
+
+export default GastoItem;
---
+++ src/components/GastoList.jsx
@@ -0,0 +1,25 @@
+import GastoItem from "./GastoItem";
+
+function GastoList({gastos, categoriaFiltro, onGastoEliminado}) {
+    const gastosFiltrados = categoriaFiltro === "" ? gastos :
+        gastos.filter((g) => g.categoria === categoriaFiltro);
+
+    if (gastosFiltrados.length === 0){
+        return <p>No hay gastos para mostrar.</p>;
+    }
+
+    return(
+         <ul>
+      {gastosFiltrados.map((gasto) => (
+        <GastoItem
+          key={gasto.id}
+          gasto={gasto}
+          onGastoEliminado={onGastoEliminado}
+        />
+      ))}
+    </ul>
+  );
+
+
+    export default GastoList;
+}
